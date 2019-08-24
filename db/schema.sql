4651451c9fd6895bc103b1cefcf8be8e1aac6a77
nick <lombardo15@temple.edu>
2019-08-24 14:02:02 -0400

    burgers

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burg
+CREATE TABLE burgers
 (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
