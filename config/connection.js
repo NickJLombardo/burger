e6f95819d67f428ede9be8a1a5804daa9451f19f
nick <lombardo15@temple.edu>
2019-08-24 13:43:35 -0400

    heroku login

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ if(process.env.JAWSDB_URL){
 connection= mysql.createConnection(process.env.JAWSDB_URL);
 }else{
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "root",
     password: "root",
     database: "burgers_db"
