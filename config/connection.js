7167f5dbcfe568a162e0564d42e347b0c9d20fee
nick <lombardo15@temple.edu>
2019-08-24 12:38:44 -0400

    login

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ connection= mysql.createConnection(process.env.JAWSDB_URL);
 }else{
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "root",
     database: "burgers_db"
